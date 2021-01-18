a8d295a7ee49ae1bd13f449145cbaf87ed92b131
m00734729 <aa4690@live.mdx.ac.uk>
2021-01-17 23:41:53 -0800

    changed file name and added header guards

---
+++ products.h
@@ -0,0 +1,16 @@
+#ifndef _PRODUCTS_H_
+#define _PRODUCTS_H_
+/*
+products.h
+Author: M00734729 <AA4690@live.mdx.ac.uk>
+Created: 17/1/2020
+Updated: 17/1/2020
+*/
+
+class Items {
+private:
+    int id;
+  std::string title;  //MOVE ALL OF THIS
+};
+
+#endif

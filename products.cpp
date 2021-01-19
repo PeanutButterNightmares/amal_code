d3e94189c68d3431bc1cbd2e96de1ec8028e2b46
m00734729 <aa4690@live.mdx.ac.uk>
2021-01-19 02:38:16 -0800

    adding class file

---
+++ products.cpp
@@ -0,0 +1,13 @@
+#include "products.h"
+
+/* 
+products.cpp
+Author: M00734729
+Created: 19/01/21
+Last Updated: 19/01/21
+*/
+products::product(){}
+
+double products::get cost(){
+	return this->cost;
+}

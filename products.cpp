a0e8029c61618e383aee2513fcb478776c577c61
m00734729 <aa4690@live.mdx.ac.uk>
2021-01-22 00:09:47 -0800

    more changes

--- products.cpp
+++ products.cpp
@@ -1,4 +1,6 @@
 #include "products.h"
+#include <string>
+#include <iostream>
 
 /* 
 products.cpp
@@ -6,8 +8,76 @@ Author: M00734729
 Created: 19/01/21
 Last Updated: 19/01/21
 */
-products::product(){}
 
-double products::get cost(){
+products::products(){};
+products::products(double cost){
+	this->cost=cost;
+}
+
+double products::getcost(){
 	return this->cost;
 }
+
+void products::setcost(double cost){
+	this->cost=cost;
+}
+	
+	Cd::Cd(){}
+	
+Cd::Cd(std::string CdName, std::string singerName, double cost)::products(cost){
+	this->CdName=CdName;
+	this->singerName=singerName;
+	}
+	
+		
+void Cd::setCdName(std::string CdName){
+	this->CdName=CdName;
+}
+
+void Cd::setsingerName(std::string singerName){
+	this->singerName=singerName;
+}
+std::string Cd::getName(){
+	return  Cdname;
+	}
+	
+std::string singer::getName(){
+	return singerName;
+}
+
+	Dvd::Dvd(){}
+Dvd::Dvd(std::string DvdName, double cost)::products(cost){
+	this->DvdName=DvdName;
+	}
+	
+void Dvd::setDvdName(std::string DvdName){
+	this->DvdName=DvdName;
+}
+
+std::string Dvd::getName(){
+	return Dvdname;
+	}
+
+Magazine::Magazine (){}
+(std::string MagazineName, double cost)::products(cost){
+	this->MagazineName=MagazineName;
+	}
+
+std::string Magazine::getName(){
+	return Magazinename;
+	}
+	
+	Book::Book(){}
+	
+Book::Book(std::string BookName, double cost)::products(cost){
+	this->BookName=BookName;
+	}
+		
+void Book::setBookName(std::string BookName){
+	this->BookName=BookName;
+}
+
+std::string Book::getName(){
+	return Bookname;
+	}
+
